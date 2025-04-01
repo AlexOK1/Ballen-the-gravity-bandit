0907db5a0d2389ba782154e54470c0bd50708729
AlexOK1 <alsa4@elev.tec.dk>
2025-04-01 11:25:54 +0200

    kamera done og enemy ai er cooked

---
+++ Assets/enemyPatrol.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyPatrol : MonoBehaviour
+{ public GameObject PointA;
+    public GameObject PointB;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currentPoint;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = PointB.transform;
+        //anim.SetBool("isMoving", true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = currentPoint.position - transform.position;
+        if(currentPoint == PointB.transform)
+        {
+            rb .velocity = new Vector2(speed, 0);
+
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointB.transform)
+        {
+            currentPoint = PointA.transform;
+        }
+
+          if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointA.transform)
+        {
+            currentPoint = PointB.transform;
+        }
+     
+    }
+    private void OawGizmos()
+    {
+        Gizmos.DrawWireSphere(PointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(PointB.transform.position, 0.5f);
+        Gizmos.DrawLine(PointA.transform.position, PointB.transform.position);
+    }
+}
